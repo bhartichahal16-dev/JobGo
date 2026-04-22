d505da54e4df63c480b079e338f249bbe534aa8a
ShubhamChawla1 <shubham07843@gmail.com>
2026-04-22 23:55:08 +0530

    Add font styling, improve JobCard button, enhance JobListing with filters and pagination, and update Home layout

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -15,7 +15,7 @@ const JobCard = ({job}) => {
         <p className='text-gray-500 text-sm mt-4' dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
         <div className='mt-4 flex gap-4 text-sm'>
             <button className='bg-blue-600 text-white px-4 py-2 rounded'>Apply now</button>
-            <button className='text-gray-500 bordr border-gray-500 rounded px-4 py-2'>Learn more</button>
+            <button className='text-gray-500 border border-gray-500 rounded px-4 py-2'>Learn more</button>
         </div>
     </div>
  )
